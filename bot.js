74f2baefb5b615a33dc6d47e49b47c31a3ab747e
Refe17 <45334088+Refe17@users.noreply.github.com>
2018-12-08 17:28:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -44,7 +44,7 @@ client.on('guildMemberAdd', member => {
                               ctx.arc(77, 145, 73, 0, Math.PI*2);
                               ctx.stroke();
                                  ctx.clip();
-                                 ctx.drawImage(ava, 5, 70, 130, 130);
+                                 ctx.drawImage(ava, 5, 70, 140, 140);
           welcomer.send({
               file: canvas.toBuffer()
           });
